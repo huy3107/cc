9f42c597a4da9079aa4e75e1cd3819eb1e18b4ca
huy3107 <124786928+huy3107@users.noreply.github.com>
2023-04-07 22:28:33 +0700

    Create PaidScriptCF4pet.lua

---
+++ PaidScriptCF4pet.lua
@@ -0,0 +1,5 @@
+MiniComet = true -- change to false if you don't want Mini Comets
+MassiveComet = true -- change to false if you don't Massive Comets
+WebHook = "https://discord.com/api/webhooks/1093900985237241896/xFZfNkdhSqTCeD2HRSjS2QZQSV5YjdF2AqL2-pNojJ3Wsv9AaA_wvM6GZ1lkddPVmPuf"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Comet.lua", true))()
